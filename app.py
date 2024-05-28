271fefa6343348c2d63b1b8065960d1506b7a05e
ConorCurleyIrl <34911584+ConorCurleyIrl@users.noreply.github.com>
2024-05-28 14:26:44 +0200

    working v_1

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
-import streamlit as st
+#terminal install the required libraries: streamlit pycaret pandas-profiling streamlit-pandas-profiling
 
-st.set_page_config(
-    layout='wide',
-    initial_sidebar_state="auto",
-    menu_items=None)
-
-import pandas as pd
-import os
+#importing libraries
 from operator import index
-
-#created file to store variables
-from variables import short_automl_desc
-
-# data viz libraries
+import streamlit as st
 import plotly.express as px
-
-# ML libraries
 from pycaret.regression import setup, compare_models, pull, save_model, load_model
-import pandas_profiling
-
-#Data profiling
+from ydata_profiling import ProfileReport
+import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 import os 
 
-#nice article here:
-#https://www.datacamp.com/tutorial/pandas-profiling-ydata-profiling-in-python-guide
+#created file to store variables
+from variables import short_automl_desc , short_class_desc, short_profile_desc, short_pycaret_desc
 
+######################################################################
+# Build the side bar - split sections to debug any issues
+######################################################################
 
-# Streamlit app code
-st.title('Automated Machine Learning App 🚀')
+st.title('Automated Machine Learning App [Classification] 🚀')
 st.write('By: Conor Curley')
+
+# using OS to check if file is up and use as needed
+if os.path.exists('uploaded_file.csv'):
+    df = pd.read_csv('uploaded_file.csv', index_col=None)
+
 
 
 with st.sidebar:
@@ -38,19 +32,24 @@ with st.sidebar:
     # Add a title to the sidebar:
     st.title('AutoML App Navigation')
     st.info('This ML app is a demo of AutoML using streamlit, pandas, pycaret and scikit-learn')
+    st.info("Note this app can only perform AutoML on classifcation problems in this version, more to come soon!")
+    st.info("What is a CLassification problem?   See 'ML Glossary' selection in the sidebar")
+
     # Add a radio button to the sidebar: 
-    choice =  st.radio('navigation menu', ['Upload Data', 'Data Profiling','Run AutoML', 'View Results','About AutoML']) 
+    choice =  st.radio('Navigation menu', ['Upload Data', 'Data Profiling','Run AutoML', 'Model download','ML Glossary']) 
 
-# using OS to check if file is up and use as needed
-if os.path.exists('uploaded_file.csv'):
-    df = pd.read_csv('uploaded_file.csv', index_col=None)
 
+
+
+######################################################################
 # lets build our upload data page
+######################################################################
+
 if choice == 'Upload Data':
     
     st.header('Select your dataset:')
     st.info('Please select a CSV file type as your dataset.')
-    st.text('')
+    
 
     # Add a file uploader to the sidebar:    
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")   
@@ -62,8 +61,7 @@ if choice == 'Upload Data':
         df.to_csv('uploaded_file.csv', index=False)
         # Display the dataset:
         st.dataframe(df)
-        st.write(df)
-        st.success('Data uploaded successfully!') 
+        #st.success('Data uploaded successfully!') 
         
     # Add common datasets
     if st.button('Links to sample datasets') == True:
@@ -78,25 +76,100 @@ if choice == 'Upload Data':
     else:    
         st.warning('No dataset uploaded yet. Please upload a dataset to continue.')
 
-             
+     
 
+######################################################################
 # lets build our data profiling page
-if choice == 'Data Profiling':
-    st.title('Automated Data Profiling:')
-    st.info("Let's see what how data looks like!")
+######################################################################
 
+if choice == 'Data Profiling':
+    st.title('Automated Data Profiling: :bar_chart:')
+    st.info("Let's see what how data looks like!")
     
+    #create profile report
+    profile_report = df.profile_report(explorative=True)
     
-    
+    #rendering the report in the streamlit app
+    st_profile_report(profile_report)
+
     st.write('Select "Run AutoML" in the sidebar to continue.')
 
 
-# lets build our About AutoML page
-if choice == 'About AutoML':
+
+######################################################################
+# lets build our Run AutoML page
+######################################################################
+#ML libraries
+import sklearn 
+from sklearn.utils import _get_column_indices
+from operator import index
+from pycaret.classification import *
+
+
+if choice == 'Run AutoML':
+    st.title('Run Automated Machine learning:')
+    st.subheader("Now let's run some AutoML magic!")
+    #create profile report
+
+    st.info('So what do you want to predict?')
+    target = st.selectbox("Select Target Variable", df.columns)
+    
+    if st.button('Train my Classification Model......Whoosh!!!'):
+        setup(df,target=target,fix_imbalance = True)
+        setup_df=pull()
+        st.info('This is the ML Experiment setup:')
+        st.dataframe(setup_df)
+        best_model = compare_models()
+        compare_df = pull()
+        st.write("Best Model: bloody Oath that's impressive ")
+        #renders the best model leaderboard: 
+        st.dataframe(compare_df)
+        st.write("Cream of the crop, that rose to the top! ")
+        best_model 
+        save_model(best_model, 'Data pipeline & best_model ')    
+
+        auc_img = plot_model(best_model, plot = 'auc')
+        # plot confusion matrix
+        cm_img = plot_model(best_model, plot = 'confusion_matrix')
+        # plot feature importance
+        features_img  = plot_model(best_model, plot = 'feature')
+        #render the images
+        st.image(features_img)
+        st.image(cm_img)
+        st.image(auc_img)
+
+            
+######################################################################
+# lets build our Model Download page
+######################################################################
+
+if choice == 'Model Download':
+    with st.spinner('Wait for it...'):
+        # Download the model as a pickle file
+        st.markdown('### [Download the model](best_model.pkl)')
+        st.success('Downloaded successfully!')
+        st.balloons()
+        st.info('Select "View Results" in the sidebar to continue.')
+
+
+######################################################################
+# lets build our ML Glossary page
+######################################################################
+
+if choice == 'ML Glossary':
     st.title('About AutoML:')
+    st.info(short_class_desc)
+    
+    st.title('Explain AutoML:')
     st.info(short_automl_desc)
+
+    st.title('Explain Pandas Profiling Report')
+    st.info(short_profile_desc)
+    
+    st.title('Pycaret')
+    st.info(short_pycaret_desc)
+    
     st.image('https://i.pinimg.com/originals/cc/32/99/cc3299350f3d91327d4a8922ecae8fb8.gif')
-    st.success('Welcome to the AutoML app!')
+    
 
   
-
