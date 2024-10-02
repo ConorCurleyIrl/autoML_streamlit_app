11f5e234e8ef56f1fe396f05bf19297316b322b0
Conor Curley <34911584+ConorCurleyIrl@users.noreply.github.com>
2024-10-02 22:19:26 +0200

    updated requirements

--- pages/Further_Info.py
+++ pages/Further_Info.py
@@ -28,6 +28,18 @@ st.subheader('App Build Notes')
 st.image(width=300, image=f'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.tenor.com%2FUg6cbVA1ZsMAAAAM%2Fdeveloper.gif&f=1&nofb=1&ipt=7285c5bfb06e6eae122b6d0a5d10980b726559be7da57a57848a303739a2738e&ipo=images')
 st.subheader('How I built this app:')
 st.info('I built this app using the Streamlit framework for the web interface and the Pycaret library for the machine learning models.')
+
+exp2 =st.expander("Some Notes & Learnings: (9-June-2024)")
+exp2.st.markdown("- Well its now live and I'm pretty happy with how it turned out. Learned a hell of alot about streamlit, pycaret and more python in general")
+exp2.st.markdown("- Few issues getting the package management right, 132 packages currently are needed. This app is now on Python 3.10 (using other versions created package conflicts) and I added the absolute minimum list of libraries in the requirements.txt so I could to reduce errors then use Pycaret and scikit-learn which have a lot of included dependencies so once you have them installed, you are good to go I've learned after many hours debugging. I also kept some libraries not pinned to a specific version and let the streamlit package manager avoid conflicts.")
+exp2.st.markdown("- Streamlit is a great tool for building simple web app with Python but it has some limitations - an example is that there is no multi-page functionality. I had to use a lot of workarounds (navigation menu) to get the app to work as I wanted but now I've the hnag of it so I think I can build more complex apps in the future.")
+exp2.st.markdown("- Userflow could be a bit better - trying to strike a balance between simplicity and showcasing the ML functionality was tricky. Ideally you would have full ML model training settings available in a sidebar to allow full customisation but I wanted to keep it simple for new users. I may add this in the future.")
+exp2.st.markdown("- Next time, I would plan out the app UX structure better next time - I had to refactor a lot of code as I went along to make it more user-friendly.")
+exp2.st.markdown("- Hosting the app on streamlit's public infastructure was tricky with the ML modelling need for compute. I had to configure and simplify the modelling process, previous ML training took 24mins! to work with the free tier limitations. But hey its free so I can't complain too much. Let's see how it goes with more users.")
+exp2.st.markdown("- I may deploy on Heroku next time for hosting as it has more flexibility and better performance. ")
+exp2.st.markdown("- All in all, I learned a lot building this app and I'm excited to build and release more ML apps.")
+exp2.st.markdown("- Thanks for checking out the app! If you have any feedback or questions, please let me know via LinkedIn.")
+
 st.subheader("Some Notes & Learnings: (9-June-2024)")
 st.markdown("- Well its now live and I'm pretty happy with how it turned out. Learned a hell of alot about streamlit, pycaret and more python in general")
 st.markdown("- Few issues getting the package management right, 132 packages currently are needed. This app is now on Python 3.10 (using other versions created package conflicts) and I added the absolute minimum list of libraries in the requirements.txt so I could to reduce errors then use Pycaret and scikit-learn which have a lot of included dependencies so once you have them installed, you are good to go I've learned after many hours debugging. I also kept some libraries not pinned to a specific version and let the streamlit package manager avoid conflicts.")
@@ -38,6 +50,8 @@ st.markdown("- Hosting the app on streamlit's public infastructure was tricky wi
 st.markdown("- I may deploy on Heroku next time for hosting as it has more flexibility and better performance. ")
 st.markdown("- All in all, I learned a lot building this app and I'm excited to build and release more ML apps.")
 st.markdown("- Thanks for checking out the app! If you have any feedback or questions, please let me know via LinkedIn.")
+
+
 
 st.divider()
 
